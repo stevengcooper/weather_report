1623188e5c7b04232afb504bac809e0b69a3b267
Steven Cooper <stevengcooper76@gmail.com>
2016-02-22 22:10:52 -0500

    Forecast outputs all days and text of the forecast

--- forecast.rb
+++ forecast.rb
@@ -8,4 +8,14 @@ class Forecast
     @results = JSON.parse(File.read("forecast.json"))
     # @forecast = HTTParty.get(http://api.wunderground.com/api/"#{ENV['WUNDERGROUND_KEY']}"/forecast10day/q/"#{@zip_code}".json)
   end
+
+  def forecast
+      forecast = []
+      @results["forecast"]["txt_forecast"]["forecastday"].each do |n|
+        day = n["title"]
+        text = n["fcttext"]
+        forecast << [day, text]
+      end
+      forecast.each {|x| puts x}
+    end
 end
