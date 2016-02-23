ee8b32c9a6499b0fae9048cd9e9e3a011fa996c3
Steven Cooper <stevengcooper76@gmail.com>
2016-02-23 01:12:18 -0500

    User class added. Command line written. Not funcitoning.

--- command_line.rb
+++ command_line.rb
@@ -1,16 +1,38 @@
 require 'byebug'
-require 'HTTParty'
-require './conditions'
-require './current_hurricane'
+require 'httparty'
+require './condition'
+# require './current_hurricane.rb'
 require './forecast'
 require './astronomy'
 require './alerts'
 
 
+puts "What City, State are you located in?"
+location = gets.chomp
 
-
+person = User.new(location)
+location = person.format_location
 
 c = Conditions.new(location)
 
 puts "Location: #{c.location}"
 puts "Current Temp (F) #{c.tempature}"
+puts "Current Wind (Mph) #{c.wind}"
+puts "Current Humidity: #{c.humidity}"
+puts "Total Precipitation Today: #{c.rain_today}"
+puts "Tempature feels like (F) #{c.feels_like}"
+
+astr = Astronomy.new(location)
+
+puts astr.sunrise
+puts astr.sunset
+
+f = Forecast(location)
+puts "Your extended forecast is"
+puts f.forecast
+
+alert = Alert.new(location)
+puts alert.alert_exists
+
+# cane = CurrentHuricane.new()
+# puts cane.current_hurricane
--- user.rb
+++ user.rb
@@ -1,4 +1,16 @@
 require 'httparty'
 
 class User
-  
+  attr_writer :location
+  def initialize(location:)
+    @location = location
+  end
+
+  def format_location(location)
+    if location == /(Durham,)\s(NC)/
+      location = "NC/Durham"
+    else location == /(Durham,)\s(North Carolina)/
+      location = "NC/Durham"
+    end
+  end
+end
